04a7223fe970227f0d93f25e6d7ee133829ad44d
Unfantazy <unliantazy@gmail.com>
2020-07-16 21:31:20 +0300

    added weekly-products slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function () {
 
+  $(".rate-star").rateYo({
+    starWidth: "15px",
+    readOnly: true,
+    normalFill: "#ffc000",
+    spacing: "4px"
+  });
+
+  $('.weekly-products__slider').slick({
+    appendArrows: $('.weekly-products__title'),
+    prevArrow: '<button type="button" class="slick-btn slick-prev lnr-chevron-left" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next lnr-chevron-right" alt=""></button>',
+  });
+
+
+
 });
